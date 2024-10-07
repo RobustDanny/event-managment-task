aca1a9c172e3f58f52d72a8c39887c905c82708b
Danny <deeabusal@gmail.com>
2024-10-07 15:35:51 -0400

    added past projects

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessages([
+                'email' => 'The credentials you entered are incorrect.',
+            ]);
+        }
+
+        if(!Auth::attempt($request->only(['email', 'password']))) {
+            throw ValidationException::withMessages([
+                'email' => 'The credentials you entered are incorrect.',
+            ]);
+        }
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('laravel_api_token')->plainTextToken
+        ]);
+    }
+}
